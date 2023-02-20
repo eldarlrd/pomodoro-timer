29292f52052353bb35b52278d687e04c178db866
eldarlrd <eldarlrd@protonmail.com>
2023-02-21 02:03:19 +0400

    feat: Toggle ticking

--- script.js
+++ script.js
@@ -8,9 +8,14 @@ const sessionDecrement = document.getElementById("session-decrement");
 const sessionLength = document.getElementById("session-length");
 const sessionIncrement = document.getElementById("session-increment");
 // Countdown
+const timeLabel = document.getElementById("timer-label");
 const timeLeft = document.getElementById("time-left");
 const startStop = document.getElementById("start_stop");
+const playPause = document.getElementById("play_pause");
 const reset = document.getElementById("reset");
+const beep = document.getElementById("beep");
+let ticking = false;
+let mode = false;
 // Timer
 const decreaseTime = elem => {
   elem.innerText > 1
@@ -24,7 +29,7 @@ const increaseTime = elem => {
     : null;
 };
 
-const updateSession = elem => {
+const updateTime = elem => {
   elem.innerText < 10
     ? timeLeft.innerText = `0${elem.innerText}:00`
     : timeLeft.innerText = `${elem.innerText}:00`;
@@ -36,21 +41,47 @@ const countdown = () => {
   let seconds = 0;
   let minutes = 0;
 
-  setInterval(() => {
-    if (prevSeconds > 0) {
+  setInterval(function() {
+    if (prevSeconds > 0 && ticking) {
       prevSeconds--;
       seconds = prevSeconds % 60;
       minutes = Math.floor(prevSeconds / 60);
       updateCounter(prevSeconds, seconds, minutes);
+    } else if (!ticking) {
+        clearInterval(this);
   }}, 1000);
 };
 
 const updateCounter = (prevSeconds, seconds, minutes) => {
   if (prevSeconds === 0) {
-    console.log(BING);
+    mode = !mode;
+    switchMode();
+    countdown();
   } minutes < 10
       ? timeLeft.innerText = `0${minutes}:${("0" + seconds).slice(-2)}`
       : timeLeft.innerText = `${minutes}:${("0" + seconds).slice(-2)}`;
+};
+
+const toggleTicking = () => {
+  ticking = !ticking;
+  breakDecrement.disabled = !breakDecrement.disabled;
+  breakIncrement.disabled = !breakIncrement.disabled;
+  sessionDecrement.disabled = !sessionDecrement.disabled;
+  sessionIncrement.disabled = !sessionIncrement.disabled;
+  ticking
+    ? playPause.src = "assets/icons/pause-solid.svg"
+    : playPause.src = "assets/icons/play-solid.svg";
+};
+
+const switchMode = () => {
+  beep.play();
+  if (mode) {
+    timeLabel.innerText = "Break";
+    updateTime(breakLength);
+  } else {
+      timeLabel.innerText = "Session";
+      updateTime(sessionLength);
+    }
 };
 
 const resetTime = () => {
@@ -69,16 +100,17 @@ breakIncrement.addEventListener("click", () => {
 
 sessionDecrement.addEventListener("click", () => {
   decreaseTime(sessionLength);
-  updateSession(sessionLength);
+  updateTime(sessionLength);
 })
 
 sessionIncrement.addEventListener("click", () => {
   increaseTime(sessionLength);
-  updateSession(sessionLength);
+  updateTime(sessionLength);
 })
 
 startStop.addEventListener("click", () => {
   countdown();
+  toggleTicking();
 })
 
 reset.addEventListener("click", () => {
